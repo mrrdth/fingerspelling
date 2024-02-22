80c33a5cf06e1cb998e47e0ec2c3cc8e5f737f6b
Meredith Lane <meredithlane91@gmail.com>
2024-02-22 15:18:44 +1100

    Migrates to typescript

---
+++ src/AlphabetMap.tsx
@@ -0,0 +1,55 @@
+import a from './assets/alphabet/a.png';
+import b from './assets/alphabet/b.png';
+import c from './assets/alphabet/c.png';
+import d from './assets/alphabet/d.png';
+import e from './assets/alphabet/e.png';
+import f from './assets/alphabet/f.png';
+import g from './assets/alphabet/g.png';
+import h from './assets/alphabet/h.png';
+import i from './assets/alphabet/i.png';
+import j from './assets/alphabet/j.png';
+import k from './assets/alphabet/k.png';
+import l from './assets/alphabet/l.png';
+import m from './assets/alphabet/m.png';
+import n from './assets/alphabet/n.png';
+import o from './assets/alphabet/o.png';
+import p from './assets/alphabet/p.png';
+import q from './assets/alphabet/q.png';
+import r from './assets/alphabet/r.png';
+import s from './assets/alphabet/s.png';
+import t from './assets/alphabet/t.png';
+import u from './assets/alphabet/u.png';
+import v from './assets/alphabet/v.png';
+import w from './assets/alphabet/w.png';
+import x from './assets/alphabet/x.png';
+import y from './assets/alphabet/y.png';
+import z from './assets/alphabet/z.png';
+
+export const alphabet: any = {
+    a: a,
+    b: b,
+    c: c,
+    d: d,
+    e: e,
+    f: f,
+    g: g,
+    h: h,
+    i: i,
+    j: j,
+    k: k,
+    l: l,
+    m: m,
+    n: n,
+    o: o,
+    p: p,
+    q: q,
+    r: r,
+    s: s,
+    t: t,
+    u: u,
+    v: v,
+    w: w,
+    x: x,
+    y: y,
+    z: z,
+};
